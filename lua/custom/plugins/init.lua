6b8876816159225a5ff53d891b16dcdda84b75b4
Kevin Zhang <kevzhang2022@gmail.com>
2025-04-08 14:32:42 -0500

    Update floaterm keybindings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -76,8 +76,10 @@ return {
   {
     'voldikss/vim-floaterm',
     init = function()
-      vim.g.floaterm_keymap_new = '<leader>n'
-      vim.g.floaterm_keymap_toggle = '<leader>t'
+      vim.g.floaterm_keymap_toggle = '<F7>'
+      vim.g.floaterm_keymap_prev = '<F8>'
+      vim.g_floaterm_keymap_next = '<F9>'
+      vim.g.floaterm_keymap_new = '<F12>'
     end,
   },
 }
