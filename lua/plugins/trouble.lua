303dcabd13cf95d314698b3f92ad93bffef59c83
kevzhang <kevzhang@ibm.com>
2025-11-26 18:36:05 -0600

    Update LSP configs

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,51 +1,32 @@
 return {
   'folke/trouble.nvim',
-  opts = {
-    modes = {
-      mydiags = {
-        mode = 'diagnostics',
-        filter = {
-          any = {
-            buf = 0,
-            {
-              severity = vim.diagnostic.severity.ERROR, -- errors only
-              -- limit to files in the current project
-              function(item)
-                return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
-              end,
-            },
-          },
-        },
-      },
-    },
-  },
+  opts = {},
   cmd = 'Trouble',
   keys = {
     {
       '<leader>q',
-      '<cmd>Trouble mydiags toggle<cr>',
-      desc = 'Diagnostics (Trouble)',
+      '<Cmd>Trouble diagnostics toggle filter.buf=0 pinned=true<CR>',
+      desc = 'Buffer diagnostics (Trouble)',
     },
     {
-      '<leader>ts',
-      '<cmd>Trouble symbols toggle pinned=true focus=false<cr>',
-      desc = 'Symbols (Trouble)',
+      'gs',
+      '<Cmd>Trouble lsp_document_symbols toggle pinned=true<CR>',
+      desc = 'LSP document symbols (Trouble)',
     },
     {
-      '<leader>tl',
-      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
+      'gl',
+      '<Cmd>Trouble lsp toggle win.position=right<CR>',
+      desc = 'LSP dump (Trouble)',
     },
     {
-      -- NOTE: this remaps the default LSP keymap for references
       'grr',
-      '<cmd>Trouble lsp_references toggle focus=false win.position=right<cr>',
-      desc = 'LSP References (Trouble)',
+      '<Cmd>Trouble lsp_references toggle win.position=right<CR>',
+      desc = 'LSP references (Trouble)',
     },
     {
-      '<leader>ti',
-      '<cmd>Trouble lsp_incoming_calls toggle focus=false win.position=right<cr>',
-      desc = 'LSP Incoming Calls (Trouble)',
+      'gd',
+      '<Cmd>Trouble lsp_definitions toggle win.position=right<CR>',
+      desc = 'LSP definitions (Trouble)',
     },
   },
 }
