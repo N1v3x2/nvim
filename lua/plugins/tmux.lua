a1bdd12637a1c6bbb382a4cffdf65e28210eabf1
Kevin Zhang <kevzhang2022@gmail.com>
2025-12-28 21:45:02 -0600

    Remove old nvim_set_keymap calls

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -15,21 +15,13 @@ return {
       },
     }
 
-    vim.api.nvim_set_keymap('n', '<M-h>', '', {
-      desc = 'Move to the left nvim window or tmux pane',
-      callback = tmux.move_left,
-    })
-    vim.api.nvim_set_keymap('n', '<M-j>', '', {
-      desc = 'Move to the lower nvim window or tmux pane',
-      callback = tmux.move_bottom,
-    })
-    vim.api.nvim_set_keymap('n', '<M-k>', '', {
-      desc = 'Move to the upper nvim window or tmux pane',
-      callback = tmux.move_top,
-    })
-    vim.api.nvim_set_keymap('n', '<M-l>', '', {
-      desc = 'Move to the right nvim window or tmux pane',
-      callback = tmux.move_right,
-    })
+    vim.keymap.set('n', '<M-h>', tmux.move_left,
+      { desc = 'Move to the left nvim window or tmux pane' })
+    vim.keymap.set('n', '<M-j>', tmux.move_bottom,
+      { desc = 'Move to the lower nvim window or tmux pane' })
+    vim.keymap.set('n', '<M-k>', tmux.move_top,
+      { desc = 'Move to the upper nvim window or tmux pane' })
+    vim.keymap.set('n', '<M-l>', tmux.move_right,
+      { desc = 'Move to the right nvim window or tmux pane' })
   end,
 }
